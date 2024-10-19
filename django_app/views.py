03df3df9166b10440f7fddf76a35fef00113efc1
Matheus Souza <99676439+SouzaMatheus1@users.noreply.github.com>
2024-10-19 18:06:02 -0300

    COmeçando front end da tela de alunos

--- django_app/views.py
+++ django_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+import csv
+import os
+from django.conf import settings
 
 # Create your views here.
 
@@ -10,3 +13,16 @@ def imagem(request):
 
 def tabela(request):
     return render(request, 'django_app/tabela.html')
+
+# funcoes de acao
+def index(request):
+    data = []
+    csv_path = os.path.join(settings.STORAGE_ROOT, 'alunos,csv')
+
+    with open(csv_path, newline='', encoding='urtf-8') as csvfile:
+        reader = csv.reader(csvfile)
+
+        for row in reader:
+            data.append(row)
+
+    return render(request, 'django_app/tabela.html', {'data':data})

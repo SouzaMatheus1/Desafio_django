ba7ad16fccf483227649997088d4c7746f53403b
Matheus Souza <99676439+SouzaMatheus1@users.noreply.github.com>
2024-10-21 10:56:41 -0300

    Finalizando exibicao de alunos

--- django_app/views.py
+++ django_app/views.py
@@ -12,17 +12,14 @@ def imagem(request):
     return render(request, 'django_app/imagem.html')
 
 def tabela(request):
-    return render(request, 'django_app/tabela.html')
-
-# funcoes de acao
-def index(request):
     data = []
-    csv_path = os.path.join(settings.STORAGE_ROOT, 'alunos,csv')
+    csv_path = os.path.join(settings.STORAGE_ROOT, 'alunos.csv')
 
-    with open(csv_path, newline='', encoding='urtf-8') as csvfile:
-        reader = csv.reader(csvfile)
-
-        for row in reader:
-            data.append(row)
-
-    return render(request, 'django_app/tabela.html', {'data':data})
+    try:
+        with open(csv_path, newline='', encoding='utf-8') as csvfile: 
+            reader = csv.reader(csvfile)
+            for row in reader:
+                data.append(row)
+    except FileNotFoundError:
+        data = []  
+    return render(request, 'django_app/tabela.html', {'data': data})
